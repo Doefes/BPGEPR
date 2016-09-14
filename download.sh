89c4acac7c85b8b255b9538a0f76d21206dd61f1
Dennis Dollee <dennisdollee@gmail.com>
2016-09-14 12:20:17 +0000

    Edit for legacy blast ~not tested~

--- download.sh
+++ download.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Blast version 2.2.25+
 
 echo -n > E0.txt
 echo -n > multi.fa
@@ -25,11 +26,11 @@ for i in ${!organisms[@]}; do
     gunzip $orgName/*.gz
     mv $orgName/*all.fa $orgName/$orgCode.fa
 
-    makeblastdb -in $orgName/$orgCode.fa -dbtype prot
-    blastp -query CAA37914.fa -db $orgName/$orgCode.fa -out $orgName/out_$orgCode.txt
-    blastp -query CAA37914.fa -db $orgName/$orgCode.fa -out $orgName/tab_$orgCode.txt -outfmt 6
+    formatdb -i $orgName/$orgCode.fa -p T
+    blastall -i CAA37914.fa -d $orgName/$orgCode.fa -o $orgName/out_$orgCode.txt -p blastp
+    blastall -i CAA37914.fa -d $orgName/$orgCode.fa -o $orgName/out_$orgCode.txt -p blastp -m8
 
     awk '{if($11 == 0.0){print $2;}}' $orgName/tab_$orgCode.txt >> E0.txt
-    cat $orgName/$orgCode.fa | awk '{if(substr($1,1,1) == ">") print $1"@"; else print $0}'| tr -d "\n" | sed 's/>/\n>/g'| egrep -f E0.txt | tr "@" "\n" >> multi.fa
+    awk '{if(substr($1,1,1) == ">") print $1"@"; else print $0}' $orgName/$orgCode.fa | tr -d "\n" | sed 's/>/\n>/g'| egrep -f E0.txt | tr "@" "\n" >> multi.fa
 
 done
