e6dccae7a7711cb62ac239700cee4f2b88b51318
Dennis Dollee <dennisdollee@gmail.com>
2016-09-16 13:09:19 +0000

    Rebased with master + tested and optimized on server

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-# Blast version 2.4.0+
+#!/bin/bash
+# Blast version 2.2.25+
 # ClustalW version 2.1
 
 echo -n > E0.txt
@@ -21,16 +21,15 @@ for i in ${!organisms[@]}; do
     orgCode=${i}
 
     mkdir $orgName
-    touch $orgName/.gitignore
-    
+
     # -r recursive | -P directory Prefix | -nd no directories | -A accept list
     wget -r -P $orgName/ -nd -A '*all.fa.gz' ftp://ftp.ensembl.org/pub/release-85/fasta/$orgName/pep/
     gunzip $orgName/*.gz
     mv $orgName/*all.fa $orgName/$orgCode.fa
 
-    makeblastdb -in $orgName/$orgCode.fa -dbtype prot
-    blastp -query CAA37914.fa -db $orgName/$orgCode.fa -out $orgName/out_$orgCode.txt
-    blastp -query CAA37914.fa -db $orgName/$orgCode.fa -out $orgName/tab_$orgCode.txt -outfmt 6
+    formatdb -i $orgName/$orgCode.fa -p T
+    blastall -i CAA37914.fa -d $orgName/$orgCode.fa -o $orgName/out_$orgCode.txt -p blastp
+    blastall -i CAA37914.fa -d $orgName/$orgCode.fa -o $orgName/tab_$orgCode.txt -p blastp -m8
 
     awk '{if($11 == 0.0){print $2;}}' $orgName/tab_$orgCode.txt >> E0.txt
     awk '{if(substr($1,1,1) == ">") print $1"@"; else print $0}' $orgName/$orgCode.fa | tr -d "\n" | sed 's/>/\n>/g'| egrep -f E0.txt | tr "@" "\n" >> multi.fa
@@ -40,13 +39,13 @@ done
 awk '{if (substr($1,1,1)==">") print ">"substr($1,15,10)"@"; else print $0}' CAA37914.fa | tr -d "\n" | tr "@" "\n" >> multi.fa
 
 
-clustalw2 -align -infile=multi.fa
-clustalw2 -bootstrap=990 -infile=multi.aln
+clustalw -align -infile=multi.fa
+clustalw -bootstrap=990 -infile=multi.aln
 
 awk '{if (substr($1,1,1) == ">"){if(length($1) > 11) {part1 = substr($1,5,4);part2 = substr($1,14,6);$1 = ">"part1 part2;}print $1;} else {print $1;}}' multi.fa > multi2.fa
 
-clustalw2 -align -infile=multi2.fa
-clustalw2 -bootstrap=990 -infile=multi2.aln
+clustalw -align -infile=multi2.fa
+clustalw -bootstrap=990 -infile=multi2.aln
 
 # Boom bekijken http://www.phylogeny.fr/
 # Welk eiwit meest gerelateerd aan sequentie
