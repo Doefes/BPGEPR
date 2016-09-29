797bc92e4761ede57cd1dfc0ff6557230520a760
Dennis Dollee <dennisdollee@gmail.com>
2016-09-29 17:02:51 +0000

    Retrieves all pathways and sorts them ascending

--- tpC/main.sh
+++ tpC/main.sh
@@ -2,11 +2,21 @@
 
 cd "$(dirname "$0")"
 
-echo -n > tsl
+echo -n > _KeggIDs
+echo -n > _PWs
 
 while read CODE
 do
-    CODE=${CODE%.*}
-    wget -O- http://rest.kegg.jp/conv/shr/ncbi-proteinid:$CODE >> tsl
+  CODE=${CODE%.*}
+  wget -O- http://rest.kegg.jp/conv/shr/ncbi-proteinid:$CODE >> _KeggIDs
 
 done < prot.bls
+
+while read KeggID
+do
+  KeggID=$(echo $KeggID | awk '{print $2}')
+  wget -O- http://rest.kegg.jp/link/pathway/$KeggID >> _PWs
+
+done < _KeggIDs
+
+sort -k2 -o _PWs _PWs
