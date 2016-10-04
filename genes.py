caab2fd06f18ef0860faefb3fb9d72cdcf520eeb
Dennis Dollee <dennisdollee@gmail.com>
2016-10-04 12:58:42 +0000

    readme test

---
+++ genes.py
@@ -0,0 +1,14 @@
+import os
+
+def genes(filenaam,bestandsnaam):
+	bestand = open(filenaam, "r")
+	lijst = bestand.read().splitlines()
+	for line in bestand:
+		lijst.append(line)
+	for i in range (1,78):
+		code = str(lijst[i])
+		os.system("""wget -i -q -O- http://www.ncbi.nlm.nih.gov/gene/?term=""" + code + """ | egrep "Gene ID" | awk -F ":" '{print $2}'| awk -F ", " '{print $1}' >> 			""" + bestandsnaam )
+def main():	
+	genes("blast_resultaat_gefilterd_rna.txt", "geneid.txt")
+
+main()
